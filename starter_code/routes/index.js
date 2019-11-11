47473636dfc73526a5c349d22b83e7558929ac42
Patrick Cho <pck.cho@gmail.com>
2019-11-11 17:49:37 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie.js')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then(documents => {
+      res.render('movies', { movies: documents })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+router.get('/movie/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(document => {
+      res.render('movie', { movie: document })
+      // res.send(document)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 module.exports = router;
